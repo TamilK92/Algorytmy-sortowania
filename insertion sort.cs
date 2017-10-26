22ee9e1ce82ff403eb83c38f349ca6ad7108a7bf
Kamil <32716238+TamilK92@users.noreply.github.com>
2017-10-26 17:28:11 +0200

    Add files via upload

---
+++ insertion sort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        public static int[] InsertionSort(int[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                while ((i > 0) && (tab[i] < tab[i - 1]))
+                {
+                    int temp = tab[i];
+                    tab[i] = tab[i - 1];
+                    tab[i-- - 1] = temp;
+                    
+                }
+            } return tab;
+        }
+        static void Main(string[] args)
+        {
+            int[]tab = new int [25];
+            Random r = new Random();
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = r.Next(100);
+            }
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(tab[i] + " ");
+            }
+            Console.WriteLine("\nposortowana:\n");
+            InsertionSort(tab);
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(tab[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ quick sort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace trajninnng
+{
+
+        class Program
+        {
+            public static void QuickSort(int[] array, int left, int right)
+            {
+                var i = left;
+                var j = right;
+                var pivot = array[(left + right) / 2];
+                while (i < j)
+                {
+                    while (array[i] < pivot) i++;
+                    while (array[j] > pivot) j--;
+                    if (i <= j)
+                    {
+                        // swap
+                        var tmp = array[i];
+                        array[i++] = array[j];  // ++ and -- inside array braces for shorter code
+                        array[j--] = tmp;
+                    }
+                }
+                if (left < j) QuickSort(array, left, j);
+                if (i < right) QuickSort(array, i, right);
+            }
+
+            static void Main(string[] args)
+            {
+                var rand = new Random();
+                var array = new int[15];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rand.Next(1000);
+                }
+                Console.WriteLine("Before: ");
+                Console.WriteLine(string.Join(" ", array));
+
+                QuickSort(array, 0, array.Length);
+
+                Console.WriteLine("Affter: ");
+                Console.WriteLine(string.Join(" ", array));
+
+                Console.ReadLine();
+            }
+        }
+    }
